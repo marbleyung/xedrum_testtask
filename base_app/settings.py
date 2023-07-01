e8f7e984462ada8290fa1b622af0276b18375c80
Max Shylnikov <raskolbrd@gmail.com>
2023-07-02 02:02:50 +0300

    bugfix

--- base_app/settings.py
+++ base_app/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 SECRET_KEY = getenv('SECRET_KEY')
-DEBUG = int(getenv('DEBUG'))
+DEBUG = bool(getenv('DEBUG'))
 ALLOWED_HOSTS = []
 
 # Application definition
